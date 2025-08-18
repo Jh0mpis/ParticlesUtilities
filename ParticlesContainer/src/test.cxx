13381799ae76d936d03866261f882a2a95a34a38
Jh0mpis <jmorenotr@unal.edu.co>
2025-08-19 00:35:46 +0200

    add: Add the test.cxx with the current test of the classes implementations.

---
+++ ParticlesContainer/src/test.cxx
@@ -0,0 +1,23 @@
+#include "../../../CarpetX/CarpetX/src/driver.hxx"
+#include "./include/Particles.hxx"
+#include "./include/ParticlesContainer.hxx"
+
+using ParticleData = Particles::PhotonsData;
+
+using PC = Containers::PhotonsContainer<ParticleData>;
+std::vector<std::unique_ptr<PC>> g_nupcs;
+
+extern "C" void test_setup(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_PARAMETERS;
+
+  for (int patch = 0; patch < CarpetX::ghext->num_patches(); ++patch) {
+    const auto &patchdata = CarpetX::ghext->patchdata.at(patch);
+    g_nupcs.push_back(std::make_unique<PC>(patchdata.amrcore.get()));
+  }
+}
+
+extern "C" void test(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_PARAMETERS;
+  g_nupcs[0]->initialize();
+  g_nupcs[0]->evolve();
+}
