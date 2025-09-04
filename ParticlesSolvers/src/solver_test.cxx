da9171ad7e358ebcc82d941c17d2f86878b59f39
Jh0mpis <jmorenotr@unal.edu.co>
2025-09-04 09:49:59 +0200

    remove!: All Particle Solvers utilities moved to other  repo

--- ParticlesSolvers/src/solver_test.cxx
+++
@@ -1,7 +0,0 @@
-#include "./include/Discretizer.hxx"
-#include "./include/Interpolator.hxx"
-#include <iostream>
-
-void hello (int argc, char *argv[]) {
-    std::cout<<"Hello world" <<std::endl;
-}
